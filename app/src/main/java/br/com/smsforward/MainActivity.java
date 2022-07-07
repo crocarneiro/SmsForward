64bfae956e6716553b78a26eb9809d948e8314e4
Carlos Rafael de Oliveira Carneiro <contato@carlos.carneiro.nom.br>
2022-07-07 00:19:11 -0300

    Created menu

--- app/src/main/java/br/com/smsforward/MainActivity.java
+++ app/src/main/java/br/com/smsforward/MainActivity.java
@@ -1,15 +1,51 @@
 package br.com.smsforward;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import android.view.MenuItem;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getLifecycle().addObserver(new MainActivityObserver(this));
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        Set<Integer> topLevelDestinations = new HashSet<>();
+        topLevelDestinations.add(R.id.homeScreen);
+
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(topLevelDestinations)
+                        .setOpenableLayout(drawerLayout)
+                        .build();
+
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment) supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/br/com/smsforward/MainActivityObserver.java
+++ app/src/main/java/br/com/smsforward/MainActivityObserver.java
@@ -2,9 +2,14 @@ package br.com.smsforward;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleEventObserver;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivityObserver implements LifecycleEventObserver {
     private AppCompatActivity activity;
@@ -15,6 +20,19 @@ public class MainActivityObserver implements LifecycleEventObserver {
 
     @Override
     public void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event) {
+        if(event.getTargetState() == Lifecycle.State.CREATED) {
+            NavigationView navigationView = activity.findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(menuItem -> {
+                NavController navController = Navigation.findNavController(activity, R.id.nav_host_fragment);
+                switch (menuItem.getItemId()) {
+                    default:
+                        break;
+                }
 
+                DrawerLayout drawerLayout = activity.findViewById(R.id.drawer_layout);
+                drawerLayout.close();
+                return false;
+            });
+        }
     }
 }
