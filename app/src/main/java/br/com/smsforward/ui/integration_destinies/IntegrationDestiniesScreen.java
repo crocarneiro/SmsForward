2bcdf7dcb2a5de33c8fe17a06842f19fbefbb97e
Carlos Rafael de Oliveira Carneiro <contato@carlos.carneiro.nom.br>
2022-07-10 00:27:05 -0300

    Created integration destinies screen layout

---
+++ app/src/main/java/br/com/smsforward/ui/integration_destinies/IntegrationDestiniesScreen.java
@@ -0,0 +1,24 @@
+package br.com.smsforward.ui.integration_destinies;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import br.com.smsforward.R;
+
+public class IntegrationDestiniesScreen extends Fragment {
+    public IntegrationDestiniesScreen() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_integration_destinies_screen, container, false);
+    }
+}
